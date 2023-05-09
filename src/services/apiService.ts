9c636a21dbdb195e3ac6cb54280d9b2ed8872f3c
gabriel <gabrielgsd03@gmail.com>
2023-05-09 13:34:55 -0300

    env 2

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as nookies from "nookies";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use((v) => {
